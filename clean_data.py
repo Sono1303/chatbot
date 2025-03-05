872f61939a797dd39d6abfcc8e45e76fa9aad6ae
Sono1303 <133033896+Sono1303@users.noreply.github.com>
2025-03-05 23:04:27 +0700

    Add files via upload

---
+++ clean_data.py
@@ -0,0 +1,19 @@
+import json 
+
+data = json.load(open('intents.json'))
+
+exist_tag = []
+cleaned_data = []
+for item in data['intents']:
+    if item['tag'] not in  exist_tag:
+        exist_tag.append(item['tag'])
+        cleaned_data.append(item)
+    else:
+        del item
+
+cleaned_json = {
+    "intents": cleaned_data
+}
+
+with open('intents.json', 'w') as f:
+    json.dump(cleaned_json, f, indent=4)
